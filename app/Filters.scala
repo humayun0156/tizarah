7741a4c99ebccb4c8f2b761266ebbcee1b97d734
Md. Humayun Kabir <humayun0156@gmail.com>
2016-10-17 09:13:25 +0600

    add some files

--- app/Filters.scala
+++ app/Filters.scala
@@ -1,17 +1,17 @@
 import com.google.inject.Inject
-import filters.ExampleFilter
+import filters.{ExampleFilter, LoggingFilter}
 import play.api.{Environment, Mode}
 import play.api.http.HttpFilters
 
 /**
   * @author Humayun
   */
-class Filters @Inject() (
-      env: Environment,
-      exampleFilter: ExampleFilter) extends HttpFilters {
+class Filters @Inject() (env: Environment,
+                         exampleFilter: ExampleFilter,
+                         loggingFilter: LoggingFilter) extends HttpFilters {
   override val filters = {
     if (env.mode ==  Mode.Dev)
-      Seq(exampleFilter)
+      Seq(exampleFilter, loggingFilter)
     else
       Seq.empty
   }
--- build.sbt
+++ build.sbt
@@ -10,6 +10,13 @@ libraryDependencies ++= Seq(
   "com.typesafe.play" %% "play-slick" % "2.0.2",
   "com.typesafe.play" %% "play-slick-evolutions" % "2.0.2",
   "com.h2database" % "h2" % "1.4.190",
+  "mysql"               % "mysql-connector-java" % "5.1.39",
+  // WebJars (i.e. client-side) dependencies
+  "org.webjars" %% "webjars-play" % "2.5.0",
+  "org.webjars" % "jquery" % "1.11.3",
+  "org.webjars" % "bootstrap" % "3.3.6" exclude("org.webjars", "jquery"),
+  "org.webjars" % "angularjs" % "1.5.5" exclude("org.webjars", "jquery"),
+  "org.webjars" % "angular-ui-bootstrap" % "1.3.2" exclude("org.webjars", "jquery"),
   specs2 % Test
 )
 
