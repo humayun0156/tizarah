44c260e46b5c1b3968e8743d259e805045e01632
Md. Humayun Kabir <humayun0156@gmail.com>
2016-10-24 09:19:27 +0600

    model class

---
+++ app/models/AccountHead.scala
@@ -0,0 +1,4 @@
+package models
+
+case class AccountHead(name: String, shopId: Long, id: Option[Long] = None)
+
---
+++ app/models/Shop.scala
@@ -0,0 +1,4 @@
+package models
+
+
+case class Shop(name: String, address: String, businessId: Long, id: Option[Long] = None)
