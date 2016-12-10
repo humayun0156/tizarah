ec802c23da50b9013abc7223b9ce96d6074b45af
Md. Humayun Kabir <humayun0156@gmail.com>
2016-12-10 17:07:33 +0600

    Cron job Stock history fix

--- app/modules/TizarahActorScheduler.scala
+++ app/modules/TizarahActorScheduler.scala
@@ -1,14 +1,20 @@
 package modules
 
+import java.sql.Timestamp
 import java.text.DateFormat
 import java.text.SimpleDateFormat
-import java.util.Date
+import java.util.{Calendar, Date}
 import javax.inject._
 
 import akka.actor._
 import dal.DataAccessLayer
+import models.StockItemHistory
+import play.api.Logger
+import play.api.libs.json.Json
+import utils.Utility._
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{Await, ExecutionContext}
 import scala.concurrent.duration._
 
 
@@ -17,13 +23,15 @@ class Scheduler @Inject() (val system: ActorSystem,
                           (implicit ec: ExecutionContext)
 {
   system.scheduler.schedule(
-    0.microseconds, 20.minutes, schedulerActor, "update")
+    0.microseconds, 10.minutes, schedulerActor, "update")
   system.scheduler.schedule(
     30.minutes, 30.days, schedulerActor, "clean")
 }
 
 @Singleton
 class SchedulerActor @Inject() (accessData: DataAccessLayer) extends Actor {
+  val logger = Logger(this.getClass)
+
   def receive = {
     case "update" => updateDB()
     case "clean" => clean()
@@ -40,13 +48,23 @@ class SchedulerActor @Inject() (accessData: DataAccessLayer) extends Actor {
     val isAvailable = before.getTime < toCheck.getTime && after.getTime > toCheck.getTime
 
     if (!isAvailable) {
-      println(new Date() + ", Time is not between 01:30 to 02:00.")
+      logger.info(new Date() + ", Time is not between 01:30 to 02:00.")
       return
     }
     //Do the thing on database
-    println("Executing query=====")
+    logger.info("=== Executing Stock history")
+    val cal: Calendar = Calendar.getInstance()
+    cal.add(Calendar.DATE, -1)
+    logger.info("Yesterday's date = "+ cal.getTime)
+    val ts = new Timestamp(cal.getTime.getTime)
+
+    val historyJson = accessData.getStockItems(None).map { res => Json.toJson(res).toString() }
+    //FIXME hardcoded shopId
+    val history = StockItemHistory(1, ts, Await.result(historyJson, 5.seconds))
+    accessData.insertStockHistory(history).foreach(id => println("HistoryId: " + id))
+
     val i = accessData.updateStockItems()
-    println(new Date() + ", updated=>" + i)
+    logger.info(new Date() + ", updated=>" + i)
   }
 
   def clean(): Unit ={
