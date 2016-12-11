e8c910f0f5cd921e5d762a2b49b67c0d09947607
Md. Humayun Kabir <humayun0156@gmail.com>
2016-12-11 22:11:37 +0600

    Improve in AuthFilter

--- app/filters/AuthFilter.scala
+++ app/filters/AuthFilter.scala
@@ -2,7 +2,9 @@ package filters
 
 import akka.stream.Materializer
 import com.google.inject.Inject
+import play.api.Logger
 import play.api.mvc.{Filter, RequestHeader, Result, Results}
+import utils.EncryptUtil
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -14,10 +16,23 @@ class AuthFilter @Inject()(implicit override val mat: Materializer,
 
   def apply(next: (RequestHeader) => Future[Result])
            (request: RequestHeader): Future[Result] = {
-    if (!request.path.contains("/login")) {
-      request.cookies.get("shopId") match {
+    if (!request.path.contains("/login") &&
+      !request.path.endsWith(".css") &&
+      !request.path.endsWith(".js") &&
+      !request.path.endsWith(".png") &&
+      !request.path.endsWith(".jpg")
+    ) {
+      Logger.logger.info("requestedPath=> " + request.path)
+      request.cookies.get("token") match {
         case Some(ck) =>
-          next(request)
+
+          val decryptToken = EncryptUtil.decrypt(ck.value)
+          if (decryptToken.contains("shopId") && decryptToken.contains("userId")) {
+            next(request)
+          } else {
+            Future.successful(Results.Redirect("/login"))
+          }
+
         case None => Future.successful(Results.Redirect("/login"))
       }
     } else {
