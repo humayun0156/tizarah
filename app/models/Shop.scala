94bc9089150c70187055426081012b21b10ebf65
Md. Humayun Kabir <humayun0156@gmail.com>
2016-12-11 10:44:04 +0600

    Case class for shopUser

--- app/models/Shop.scala
+++ app/models/Shop.scala
@@ -2,3 +2,8 @@ package models
 
 
 case class Shop(name: String, address: String, businessId: Long, id: Option[Long] = None)
+
+case class WorkingShop(shopId: Long, userId: Long)
+
+case class ShopUser(shopName: String, address: String, shopId: Long, userId: Long,
+                    userName: String, displayName: String)
