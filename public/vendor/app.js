4a4094b6b517d56f2044329486109d00312ac6d8
Md. Humayun Kabir <humayun0156@gmail.com>
2016-12-12 08:37:33 +0600

    Fix to get date

--- public/vendor/app.js
+++ public/vendor/app.js
@@ -253,7 +253,7 @@ app.filter('dateFormat', function() {
         var date = new Date(input);
         var year = date.getFullYear();
         var month = date.getMonth() + 1;
-        var day = date.getDay();
+        var day = date.getDate();
         return day + "/" + month + "/" + year;
     };
 });
